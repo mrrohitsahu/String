b25e8037fcef3809696aaf8135c88a8d40bc9feb
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:49:30 +0530

    Add files via upload

---
+++ uplp.java
@@ -0,0 +1,8 @@
+public class uplp {
+ public static void main(String[] args) {
+   String s="hElLO i aM rOhIT";
+   System.out.println(s.toUpperCase());
+   System.out.println(s.toLowerCase());
+}
+   
+}
