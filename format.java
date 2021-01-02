de5174e1452fe0fb24ab7164c394a04347edfa6c
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:58:38 +0530

    Add files via upload

---
+++ format.java
@@ -0,0 +1,10 @@
+public class format {
+public static void main(String[] args) {
+   String s1="This is Rohit";
+   String fx1=String.format("%s",s1);
+   String fx2=String.format("%6f", 123.456 );
+   System.out.println(fx1);
+   System.out.println(fx2);
+}
+    
+}
