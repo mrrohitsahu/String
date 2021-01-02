292a4d4f6dd6edf97d35e6143945d8d0698dbfdd
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:54:44 +0530

    Add files via upload

---
+++ equals.java
@@ -0,0 +1,11 @@
+public class equals {
+public static void main(String[] args) {
+   String s1="Hii";
+   String s2="Hii";
+   String s3="Rohit";
+   System.out.println(s1.equals(s2));
+   System.out.println(s2.equals(s3));
+   
+}
+    
+}
