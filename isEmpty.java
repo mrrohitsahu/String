4f8d9fb4d7cf8678dc1e734d60361bf1f05f9a6a
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:47:53 +0530

    Add files via upload

---
+++ isEmpty.java
@@ -0,0 +1,11 @@
+public class isEmpty {
+ public static void main(String[] args) {
+   String s1="Rohit";
+   String s2="";
+   String s3="Sahu";
+   System.out.println(s1.isEmpty());
+   System.out.println(s2.isEmpty());
+   System.out.println(s3.isEmpty());
+}
+    
+}
