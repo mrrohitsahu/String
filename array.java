efe0cf7f5a600d2d146d01ffa3b0f96c7786797a
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:50:38 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,19 @@
+public class array {
+public static void main(String[] args) {
+   char[]kid={
+       'R',
+       'O',
+       'H',
+       'I',
+       'T',
+       ' ',
+       'S',
+       'A',
+       'H',
+       'U'
+   };
+   String kidS=new String(kid);
+   System.out.println(kidS);
+}
+    
+}
