88d81ee893a5e918886875a74adce4c197d0c175
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:53:13 +0530

    Add files via upload

---
+++ contains.java
@@ -0,0 +1,10 @@
+public class contains {
+public static void main(String[] args) {
+   String s="Rohit";
+   System.out.println(s.contains("Ro"));
+   System.out.println(s.contains("an"));
+   System.out.println(s.contains("it"));
+   
+}
+    
+}
