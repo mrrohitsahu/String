5cf53f15fd483c0ab72d6d0ccb0248b5e8e22210
mrrohitsahu <76870190+mrrohitsahu@users.noreply.github.com>
2021-01-02 21:53:58 +0530

    Add files via upload

---
+++ endsWith.java
@@ -0,0 +1,10 @@
+public class endsWith {
+public static void main(String[] args) {
+   String s="HelloIamRohit";
+   System.out.println(s.endsWith("Iam"));
+   System.out.println(s.endsWith("hit"));
+   System.out.println(s.endsWith("t"));
+   
+}
+    
+}
